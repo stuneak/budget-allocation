d1629def1a5ecbddb512a6596d46cc9f1988700a
Abu Shamsutdinov <yakotikadot@gmail.com>
2018-03-15 22:21:09 +0300

    fix content

--- src/app/pages/Dashboard/index.js
+++ src/app/pages/Dashboard/index.js
@@ -8,12 +8,36 @@ const { Content } = Layout;
 
 class Dashboard extends React.Component {
   render () {
-    const { userData: { budget }, logout, username } = this.props;
+    const {
+      userData: { budget, shoppingList, categories },
+      logout,
+      username
+    } = this.props;
+
+    const renderShoppingList = shoppingList.map((item, idx) => (
+      <li key={idx}>
+        <p>categories: {item.categories}</p>
+        <p>name: {item.label}</p>
+        <p>price: {item.price}</p>
+      </li>
+    ));
+
+    const renderCategories = categories.map((item, idx) => (
+      <li key={idx}>name: {item}</li>
+    ));
+
     return (
       <StyledLayout>
         <SiderBar budget={budget} handleLogOut={logout} username={username} />
         <Layout>
-          <Content>Content</Content>
+          <Content>
+            <ul>
+              <h3>Shopping list{renderShoppingList}</h3>
+            </ul>
+            <ul>
+              <h3>Categories list{renderCategories}</h3>
+            </ul>
+          </Content>
         </Layout>
       </StyledLayout>
     );
