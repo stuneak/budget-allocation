b8deb5bbebdfe0fd18926680b562901cb738751f
Abu Shamsutdinov <yakotikadot@gmail.com>
2018-03-17 13:24:41 +0300

    added ability to save data (server)

--- server/api/userData.js
+++ server/api/userData.js
@@ -6,8 +6,31 @@ import Config from '../config';
 export default resource({
   /** POST **/
   create ({ headers: { authorization }, body }, res) {
+    let params = {};
+    if (body.budget >= 0) {
+      params.budget = body.budget;
+    }
+    if (body.categories) {
+      if (Array.isArray(JSON.parse(body.categories))) {
+        params.categories = JSON.parse(body.categories);
+      } else {
+        res
+          .status(404)
+          .send({ success: false, message: `categories is not array` });
+      }
+    }
+    if (body.shoppingList) {
+      if (Array.isArray(JSON.parse(body.shoppingList))) {
+        params.shoppingList = JSON.parse(body.shoppingList);
+      } else {
+        res
+          .status(404)
+          .send({ success: false, message: `shoppingList is not array` });
+      }
+    }
+
     checkToken(authorization, res, decoded => {
-      UserDataModel.findByIdAndUpdate(decoded._id, body)
+      UserDataModel.findByIdAndUpdate(decoded._id, params)
         .then(() => res.send({ success: true, message: 'Update success' }))
         .catch(error =>
           res.status(404).send({ success: false, message: error })
