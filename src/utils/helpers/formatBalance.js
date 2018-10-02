265021e6db30574a433b0655129750d885a04b53
Mike Syomin <lebatmann@gmail.com>
2018-10-02 23:17:12 +0300

    Update formatBalance.js (#1)
    The usage of `var`'s in 2k18 isn't good idea. Be on the edge with ES6 :sob:

--- src/utils/helpers/formatBalance.js
+++ src/utils/helpers/formatBalance.js
@@ -1,5 +1,5 @@
 export const formatBalance = x => {
-  var parts = x.toString().split('.');
+  let parts = x.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
 };
